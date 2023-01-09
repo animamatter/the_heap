ec513bffb78435432c92cec795e596ede57dc974
Nuno Ribeiro <nmpribeiro@gmail.com>
2023-01-09 11:53:09 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# The Heap
+
+## Resources
+
+### Videos
+
+- [Building a blockchain info app in rust 01](https://youtu.be/TUzKN9xFKJs)
+- [Build a cryptocurrency! - Blockchain in Rust #01: Blocks & Hashing](https://youtu.be/vJdT05zl6jk)
+
+### Articles
+
+- [How to build a blockchain with rust](https://www.leewayhertz.com/how-to-build-a-blockchain-with-rust/)
+- [How to build a blockchain in rust](https://blog.logrocket.com/how-to-build-a-blockchain-in-rust/)
---
+++ src/block.rs
@@ -0,0 +1,80 @@
+use hex;
+use std::fmt::{self, Debug, Formatter};
+
+use crate::difficulty_bytes_as_u128;
+
+use super::{u128_bytes, u32_bytes, u64_bytes, BlockHash, Hashable};
+
+pub struct Block {
+    pub index: u32,
+    pub timestamp: u128,
+    pub hash: BlockHash,
+    pub prev_block_hash: BlockHash,
+    pub nonce: u64,
+    pub payload: String,
+    pub difficulty: u128,
+}
+
+impl Debug for Block {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        write!(
+            f,
+            "Block[{}]: {} at: {} with: {}",
+            &self.index,
+            &hex::encode(&self.hash),
+            &self.timestamp,
+            &self.payload
+        )
+    }
+}
+
+impl Block {
+    pub fn new(
+        index: u32,
+        timestamp: u128,
+        prev_block_hash: BlockHash,
+        nonce: u64,
+        payload: String,
+        difficulty: u128,
+    ) -> Self {
+        Block {
+            index,
+            timestamp,
+            hash: vec![0; 32],
+            prev_block_hash,
+            nonce,
+            payload,
+            difficulty,
+        }
+    }
+
+    pub fn mine(&mut self) {
+        for none_attempt in 0..(u64::max_value()) {
+            self.nonce = none_attempt;
+            let hash = self.hash();
+            if check_difficulty(&hash, self.difficulty) {
+                self.hash = hash;
+                return;
+            }
+        }
+    }
+}
+
+impl Hashable for Block {
+    fn bytes(&self) -> Vec<u8> {
+        let mut bytes = vec![];
+
+        bytes.extend(&u32_bytes(&self.index));
+        bytes.extend(&u128_bytes(&self.timestamp));
+        bytes.extend(&self.prev_block_hash);
+        bytes.extend(&u64_bytes(&self.nonce));
+        bytes.extend(self.payload.as_bytes());
+        bytes.extend(&u128_bytes(&self.difficulty));
+
+        bytes
+    }
+}
+
+pub fn check_difficulty(hash: &BlockHash, difficulty: u128) -> bool {
+    difficulty > difficulty_bytes_as_u128(&hash)
+}
